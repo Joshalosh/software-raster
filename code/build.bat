5cd696cb3a99f62c7af8daffcfc046e23d781fc9
Joshalosh <josh.jessopsmith@gmail.com>
2025-11-21 19:47:32 +1100

    Draw black and white to the screen when it is resized

--- code/build.bat
+++ code/build.bat
@@ -2,7 +2,7 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi ..\code\raster.cpp user32.lib
+cl -Zi ..\code\raster.cpp user32.lib gdi32.lib
 popd
 
 
--- code/raster.cpp
+++ code/raster.cpp
@@ -29,15 +29,62 @@ void ResizeDIBSection(int width, int height) {
 #endif
 
 LRESULT CALLBACK MainWindowCallback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
+    LRESULT result = 0;
+    switch (message) {
+        case WM_SIZE:        OutputDebugStringA("WM_SIZE\n");               break;
+        case WM_DESTROY:     OutputDebugStringA("WM_DESTROY\n");            break;
+        case WM_CLOSE:       OutputDebugStringA("WM_CLOSE\n");              break;
+        case WM_ACTIVATEAPP: OutputDebugStringA("WM_ACTIVATEAPP\n");        break;
+        case WM_PAINT: {  
+            PAINTSTRUCT paint;
+            HDC device_context = BeginPaint(window, &paint);
+            int x = paint.rcPaint.left;
+            int y = paint.rcPaint.top;
+            int width = paint.rcPaint.right - paint.rcPaint.left;
+            int height = paint.rcPaint.bottom - paint.rcPaint.top;
+            static DWORD operation = WHITENESS;
+            PatBlt(device_context, x, y, width, height, operation);
+            if (operation == WHITENESS) {
+                operation = BLACKNESS;
+            } else { 
+                operation = WHITENESS;
+            }
+            EndPaint(window, &paint);
+        } break;
+        default: result = DefWindowProc(window, message, w_param, l_param); break;
+    }
+    return result;
 }
 
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR command_line, int show_code) {
     WNDCLASS window_class = {};
-    window_class.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
     window_class.lpfnWndProc = MainWindowCallback;
-    window_class.hinstance = instance;
+    window_class.hInstance = instance;
     // window_class.hIcon;
     window_class.lpszClassName = "RasterWindowClass";
+
+    if (RegisterClass(&window_class)) { 
+        HWND window_handle = CreateWindowEx(0, window_class.lpszClassName, "Rasteriser", 
+                                            WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT,
+                                            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+                                            0, 0, instance, 0);
+        if (window_handle) {
+            for(;;) {
+                MSG message; 
+                BOOL message_result = GetMessage(&message, 0, 0, 0);
+                if (message_result > 0) {
+                    TranslateMessage(&message);
+                    DispatchMessage(&message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO: Do some logging here for failed window handle.
+        }
+    } else {
+            // TODO: Do some logging here for failed window class.
+    }
     return 0;
 }
 
